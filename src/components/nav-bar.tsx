b1bddcd6551d7662795675a7b278186b6d6d054a
kvtemadden <katy.madden99@gmail.com>
2024-06-05 12:13:07 +0100

    Refactor nav-bar component to include a link to the homepage

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -8,12 +8,15 @@ import {
 } from "@/components/ui/navigation-menu";
 
 import { Logo } from "./logo";
+import Link from "next/link";
 
 export const NavBar = () => {
   return (
     <div className="flex w-full flex-col py-8">
       <div className="flex h-full w-full items-center justify-center p-4">
-        <Logo />
+        <Link href="/" shallow className="cursor-pointer">
+          <Logo />
+        </Link>
       </div>
       <NavigationMenu className="hidden w-full max-w-full">
         <NavigationMenuList className="flex w-full items-center justify-center gap-4">
